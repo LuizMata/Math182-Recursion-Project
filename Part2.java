354659a5363b4842571633bec99eebcabc9d6366
Luiz Mata Lopez <70717884+LuizMata@users.noreply.github.com>
2022-05-03 12:57:40 -0400

    Add files via upload

--- Part2.java
+++ Part2.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 public class Part2 {
     static int[] storage = new int[100];
-    static int i = 2;
+    static int c1 = 1;
+    static int c2 = 1;
+    static int i = 1;
+    static int number = 1;
     /*pseudocode
              1. Start with 2 givens, in this case we start with 2 as our first value
                 and 3 as our second value.
@@ -12,7 +15,7 @@ public class Part2 {
     public static void main(String[] str){
         Scanner input = new Scanner(System.in);
         System.out.print("Which F number do you need?: ");
-         Fibonacci(input.nextInt());
+         FibonacciNoStorage(input.nextInt());
 
     }
 
@@ -21,9 +24,9 @@ public class Part2 {
      * @param F the fibonacci number F(n)
      */
     public static void Fibonacci(int F){
-        int number=2;
-        storage[0]=2;
-        storage[1]=3;
+        int number=1;
+        storage[0]=1;
+        storage[1]=1;
         storage[i] = storage[i-1]+ storage[i-2];
         number = storage[i-1];
 
@@ -34,6 +37,25 @@ public class Part2 {
             i++;
             Fibonacci(F);
         }
+    }
+
+    //Fibonacci sequence calculator using only static variables
+    //Reduces amount of memory used
+
+    public static void FibonacciNoStorage(int F){
+        number = c2+c1;
+        c1=c2;
+        c2 = number;
+
+        if(i == F){
+            System.out.println(number -c1);
+        }
+        else{
+            i++;
+            FibonacciNoStorage(F);
+        }
+
+
     }
 
 }
